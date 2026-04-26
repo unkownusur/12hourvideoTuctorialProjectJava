c08998040cfe87f7eec4d9bccf731b4ffcfcf571
unkownusur <mudpuppygamer@gmail.com>
2026-04-26 14:07:45 -0400

    membership cost calculator

--- src/Main.java
+++ src/Main.java
@@ -2,55 +2,26 @@ import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        String item;
-        double price;
-        double amount;
-        char currencey;
-        double total;
-        double pay = 0;
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("What item would you like to buy?: ");
-        item = scanner.nextLine();
+        double cost;
+        int amount;
+        double years;
+        double finalCost;
 
-        System.out.print("What is the price for each?: ");
-        price = scanner.nextDouble();
+        System.out.print("what is the cost of the membership per year?: ");
+        cost = scanner.nextDouble();
 
-        System.out.print("How many would you like?: ");
-        amount = scanner.nextDouble();
+        System.out.print("How many memberships do you want?: ");
+        amount = scanner.nextInt();
 
-        System.out.print("What is your currencey?: ");
-        currencey = scanner.next().charAt(0);
+        System.out.print("how many years do you want the membership for?: ");
+        years = scanner.nextDouble();
 
-        total = Math.ceil((price * amount)*100);
-        total /= 100;
+        finalCost = cost * years * amount;
 
-        System.out.println("You have bought " + amount + " " + item + "/s");
-        System.out.println("Your total is " + currencey + total);
-        
-        purchase(total, pay);
+        System.out.printf("The cost will be $%.2f", finalCost);
 
         scanner.close();
-
-    }
-
-    static void purchase(double total, double pay) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("How much money do you want to pay?: ");
-        pay = scanner.nextDouble();
-
-        if (pay > total) {
-            System.out.println("here is your change: " + (pay - total));
-            System.out.println("thank you for shopping!");
-        } else if (pay == total) {
-            System.out.println("thank you for shopping!");
-        } else {
-            System.out.println("you need to pay more! here is how much more you need: " + (total - pay));
-            total -= pay;
-            purchase(total, pay);
-        }
     }
 }
